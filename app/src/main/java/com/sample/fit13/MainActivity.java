a49aebd5568619d98f3ee6bb64744990cd395120
Bryan Wade <bwade897@gmail.com>
2021-04-25 16:48:33 -0400

    Test Bryan

--- app/src/main/java/com/sample/fit13/MainActivity.java
+++ app/src/main/java/com/sample/fit13/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //Hello
     Button button;
     Button button2;
     Button button3;
